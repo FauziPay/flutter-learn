7f7bf18ac9227a8a8a52648d79bbf437b065be15
Achmad Nur Fauzi <61571729+FauziPay@users.noreply.github.com>
2023-01-31 16:09:48 +0700

    update fundamental parsing json

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_learn_parse/screen/products_overview_screen.dart';
+import 'package:flutter_learn_parse/screen/fundamental_parsing_json_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: ProductOverviewBlocScreen(),
+        body: ParsingJsonScreen(),
       ),
     );
   }
--- lib/screen/fundamental_parsing_json_screen.dart
+++ lib/screen/fundamental_parsing_json_screen.dart
@@ -3,13 +3,17 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class ParsingJsonScreen extends StatelessWidget {
+  const ParsingJsonScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: ParsingJsonScreenBody(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Fundamental Parsing Json"),
+        centerTitle: true,
+      ),
+      body: const ParsingJsonScreenBody(),
     );
   }
 }
@@ -33,6 +37,7 @@ class _ParsingJsonScreenBodyState extends State<ParsingJsonScreenBody> {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ElevatedButton(
             onPressed: () {},
@@ -41,6 +46,15 @@ class _ParsingJsonScreenBodyState extends State<ParsingJsonScreenBody> {
           const SizedBox(
             height: 12.0,
           ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              "no data",
+              style: TextStyle(
+                fontSize: 22.0,
+              ),
+            ),
+          )
         ],
       ),
     );
